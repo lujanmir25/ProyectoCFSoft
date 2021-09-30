e0838e2a252e7cc6bd4f090eda5b8f0574349e46
lujan <lujanmir25>
2021-09-29 21:45:14 -0400

    campo existencia

--- productos/models.py
+++ productos/models.py
@@ -17,7 +17,8 @@ class Producto(ClaseModelo):
 
     marca = models.CharField(max_length=30)
 
-    precio_venta = models.IntegerField(blank=False)
+    precio_venta = models.FloatField(default=0)
+    existencia = models.IntegerField (default=0)
 
     def __str__(self):
         return '{}'.format(self.product_name)
