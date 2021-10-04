c17794c8fddb2a3fe520a932ea1cbc01b594f2e1
paulo-arias <ariaspaulo21@gmail.com>
2021-10-03 22:36:36 -0300

    branch update ventas

--- ventas/urls.py
+++ ventas/urls.py
@@ -2,10 +2,11 @@
 from django.urls import path,include
 
 #Local
-from ventas.views import FacturaView
+from ventas.views import FacturaView,facturas
 
 
 
 urlpatterns = [
 	path('ventas/',FacturaView.as_view(), name='factura_list'),
+	path('ventas/new',facturas, name='factura_new'),
 ]
--- ventas/views.py
+++ ventas/views.py
@@ -1,13 +1,18 @@
 #Django
-from django.shortcuts import render
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import generic
+from django.contrib.auth.decorators import login_required,permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib import messages
 from django.urls import reverse_lazy
+from datetime import datetime
+
 
 #Local
 from ventas.models import FacturaEnc, FacturaDet
+from Clientes.models import Cliente
+from Clientes.forms import ClienteForm
+from productos.models import Producto
 
 
 class FacturaView(LoginRequiredMixin, PermissionRequiredMixin, generic.ListView):
@@ -16,3 +21,20 @@ class FacturaView(LoginRequiredMixin, PermissionRequiredMixin, generic.ListView)
 	template_name = "ventas/factura_list.html"
 	context_object_name = "obj"
 	login_url = 'bases:login'
+
+@login_required(login_url='/login/')
+@permission_required('ventas.change_facturasenc')
+
+def facturas(request, id=None):
+	template_name = 'ventas/facturas.html'
+	encabezado = {
+		'fecha':datetime.today()
+	}
+	detalle = {}
+	clientes = Cliente.objects.filter
+
+
+	contexto={"enc":encabezado,"det":detalle,"clientes":clientes}
+
+
+	return render(request,template_name, contexto)
