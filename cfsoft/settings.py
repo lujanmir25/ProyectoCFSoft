456f8fdf66355b088bde468dc171bca8473f0049
lujan <lujanmir25>
2021-12-06 00:45:19 -0300

    setting

--- cfsoft/settings.py
+++ cfsoft/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'cfsoft.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+""" 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -111,7 +111,7 @@ DATABASES = {
         'HOST':'localhost',
     }
 }
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
